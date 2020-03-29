83b9790c50d932a94af60f6a2baea38767b4568f
milan.zivanov <milan.zivanov@gmail.com>
2020-03-29 22:43:46 +0200

    design gallery and links 3

--- src/js/main.js
+++ src/js/main.js
@@ -22,6 +22,35 @@ $(document).ready(function() {
         nextArrow: $(".right-nav-custom"),
         appendDots: $(".my-pagination"),
         cssEase: 'ease-out',
+
+        responsive: [
+            // {
+            //   breakpoint: 1024,
+            //   settings: {
+            //     slidesToShow: 3,
+            //     slidesToScroll: 3,
+            //     infinite: true,
+            //     dots: true
+            //   }
+            // },
+            // {
+            //   breakpoint: 600,
+            //   settings: {
+            //     slidesToShow: 2,
+            //     slidesToScroll: 2
+            //   }
+            // },
+            // {
+            //   breakpoint: 480,
+            //   settings: {
+            //     slidesToShow: 1,
+            //     slidesToScroll: 1
+            //   }
+            // }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
     });
 
 
