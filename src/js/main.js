97a0f7815b2a900667950991f15d2e213c9ebcb8
milan.zivanov <milan.zivanov@gmail.com>
2020-04-17 22:14:55 +0200

    first commit new design main page in construction 1

--- src/js/main.js
+++ src/js/main.js
@@ -136,35 +136,35 @@ overlayClose.addEventListener('click', close);
 
 // modal 
 // Get modal element
-const modal = document.getElementById('simpleModalJs');
-// Get open modal button
-const modalBtn = document.getElementById('modalBtnJs');
-// Get close button
-const closeBtn = document.querySelector('.closeBtn');
+// const modal = document.getElementById('simpleModalJs');
+// // Get open modal button
+// const modalBtn = document.getElementById('modalBtnJs');
+// // Get close button
+// const closeBtn = document.querySelector('.closeBtn');
 
-// Listen for open click
-modalBtn.addEventListener('click', openModal);
-// Listen for outside click
-window.addEventListener('click', outsideClick);
-// Listen for close click
-closeBtn.addEventListener('click', closeModal);
+// // Listen for open click
+// modalBtn.addEventListener('click', openModal);
+// // Listen for outside click
+// window.addEventListener('click', outsideClick);
+// // Listen for close click
+// closeBtn.addEventListener('click', closeModal);
 
-// Function to open modal
-function openModal() {
-    modal.style.display = 'flex';
-}
+// // Function to open modal
+// function openModal() {
+//     modal.style.display = 'flex';
+// }
 
-// Function to close modal
-function closeModal(){
-    modal.style.display = 'none';
-}
+// // Function to close modal
+// function closeModal(){
+//     modal.style.display = 'none';
+// }
 
-// // Function to close modal if outside click
-function outsideClick(e) {
-    if (e.target === modal) {
-        modal.style.display = 'none';
-    }
-}
+// // // Function to close modal if outside click
+// function outsideClick(e) {
+//     if (e.target === modal) {
+//         modal.style.display = 'none';
+//     }
+// }
 
 
 
