3a2fa02b8e8b5fe039ea08efde09ac8b1c32f835
milan.zivanov <milan.zivanov@gmail.com>
2020-11-09 13:54:37 +0100

    from desktop frsto to mobile first header

--- src/js/main.js
+++ src/js/main.js
@@ -21,16 +21,16 @@ $(document).ready(function() {
     // }
 
     // Slick
-    $('.slick-slider').slick({
-        arrows: true,
-        dots: false,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        adaptiveHeight: true,
-        prevArrow: $(".left-nav-custom"),
-        nextArrow: $(".right-nav-custom"),
-        cssEase: 'ease-out'
-    });
+    // $('.slick-slider').slick({
+    //     arrows: true,
+    //     dots: false,
+    //     autoplay: true,
+    //     autoplaySpeed: 5000,
+    //     adaptiveHeight: true,
+    //     prevArrow: $(".left-nav-custom"),
+    //     nextArrow: $(".right-nav-custom"),
+    //     cssEase: 'ease-out'
+    // });
 
     // open mobil menu and hamburger btn
     $(".botton-nav").on("click", function(e) {
@@ -39,84 +39,84 @@ $(document).ready(function() {
 
 
     // scroll to section
-    $('a[href^="#"]').bind('click', function(e) {
-        e.preventDefault(); 
+    // $('a[href^="#"]').bind('click', function(e) {
+    //     e.preventDefault(); 
 
-        var target = $(this).attr("href");
+    //     var target = $(this).attr("href");
 
-        $('html, body').stop().animate({
-            scrollTop: $(target).offset().top
-        }, 600, function() {
-            location.hash = target; 
-        });
+    //     $('html, body').stop().animate({
+    //         scrollTop: $(target).offset().top
+    //     }, 600, function() {
+    //         location.hash = target; 
+    //     });
 
-        $(".botton-nav, .header-nav__container").removeClass("active");
+    //     $(".botton-nav, .header-nav__container").removeClass("active");
 
-        return false;
-    });
+    //     return false;
+    // });
 
     // btt
-    if ($('.back-to-top').length) {
-        let scrollTrigger = 100, // px
-        backToTop = function () {
-            var scrollTop = $(window).scrollTop();
-            if (scrollTop > scrollTrigger) {
-                $('.back-to-top').addClass('show');
-            } else {
-                $('.back-to-top').removeClass('show');
-            }
-        };
-        backToTop();
-        $(window).on('scroll', function () {
-            backToTop();
-        });
-        $('.back-to-top').on('click', function (e) {
-            e.preventDefault();
-            $('html,body').animate({
-                scrollTop: 0
-            }, 500);
-        });
-    }
+    // if ($('.back-to-top').length) {
+    //     let scrollTrigger = 100, // px
+    //     backToTop = function () {
+    //         var scrollTop = $(window).scrollTop();
+    //         if (scrollTop > scrollTrigger) {
+    //             $('.back-to-top').addClass('show');
+    //         } else {
+    //             $('.back-to-top').removeClass('show');
+    //         }
+    //     };
+    //     backToTop();
+    //     $(window).on('scroll', function () {
+    //         backToTop();
+    //     });
+    //     $('.back-to-top').on('click', function (e) {
+    //         e.preventDefault();
+    //         $('html,body').animate({
+    //             scrollTop: 0
+    //         }, 500);
+    //     });
+    // }
 
     // accordian education
-    $(function() {
-        $('.acc_ctrl').on('click', function(e) {
-          e.preventDefault();
-          if ($(this).hasClass('active')) {
-            $(this).removeClass('active');
-            $(this).next()
-            .stop()
-            .slideUp(300);
-          } else {
-            $(this).addClass('active');
-            $(this).next()
-            .stop()
-            .slideDown(300);
-          }
-        });
-      });
+    // $(function() {
+    //     $('.acc_ctrl').on('click', function(e) {
+    //       e.preventDefault();
+    //       if ($(this).hasClass('active')) {
+    //         $(this).removeClass('active');
+    //         $(this).next()
+    //         .stop()
+    //         .slideUp(300);
+    //       } else {
+    //         $(this).addClass('active');
+    //         $(this).next()
+    //         .stop()
+    //         .slideDown(300);
+    //       }
+    //     });
+    //   });
 
 });
 
 ////////// wes bos grid gallery
-const gallery = document.querySelector('.gallery-box');
-const overlay = document.querySelector('.overlay-box');
-const overlayImage = overlay.querySelector('img');
-const overlayClose = overlay.querySelector('.close');
+// const gallery = document.querySelector('.gallery-box');
+// const overlay = document.querySelector('.overlay-box');
+// const overlayImage = overlay.querySelector('img');
+// const overlayClose = overlay.querySelector('.close');
 
-function handleClick(e) {
-    const src = e.currentTarget.querySelector('img').src;
-    overlayImage.src = src;
-    overlay.classList.add('open');
-}
+// function handleClick(e) {
+//     const src = e.currentTarget.querySelector('img').src;
+//     overlayImage.src = src;
+//     overlay.classList.add('open');
+// }
 
-function close() {
-    overlay.classList.remove('open');
-}
+// function close() {
+//     overlay.classList.remove('open');
+// }
 
-const items = document.querySelectorAll('.item-flex');
-items.forEach(item => item.addEventListener('click', handleClick));
-overlayClose.addEventListener('click', close);
+// const items = document.querySelectorAll('.item-flex');
+// items.forEach(item => item.addEventListener('click', handleClick));
+// overlayClose.addEventListener('click', close);
 
 
 
