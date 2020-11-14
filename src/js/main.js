90e142ab23eb0ff1c78edc0deae15fd8a588668d
milan.zivanov <milan.zivanov@gmail.com>
2020-11-14 15:47:08 +0100

    mobile first gallery-box app-box contact-box and footer still in progress

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+// jQuery(function() { });
+
 $(document).ready(function() {
     'use strict';
 
@@ -37,46 +39,45 @@ $(document).ready(function() {
         $(".botton-nav, .header-nav__container").toggleClass("active");
     });
 
-
     // scroll to section
-    // $('a[href^="#"]').bind('click', function(e) {
-    //     e.preventDefault(); 
+    $('a[href^="#"]').bind('click', function(e) {
+        e.preventDefault(); 
 
-    //     var target = $(this).attr("href");
+        var target = $(this).attr("href");
 
-    //     $('html, body').stop().animate({
-    //         scrollTop: $(target).offset().top
-    //     }, 600, function() {
-    //         location.hash = target; 
-    //     });
+        $('html, body').stop().animate({
+            scrollTop: $(target).offset().top
+        }, 600, function() {
+            location.hash = target; 
+        });
 
-    //     $(".botton-nav, .header-nav__container").removeClass("active");
+        $(".botton-nav, .header-nav__container").removeClass("active");
 
-    //     return false;
-    // });
+        return false;
+    });
 
     // btt
-    // if ($('.back-to-top').length) {
-    //     let scrollTrigger = 100, // px
-    //     backToTop = function () {
-    //         var scrollTop = $(window).scrollTop();
-    //         if (scrollTop > scrollTrigger) {
-    //             $('.back-to-top').addClass('show');
-    //         } else {
-    //             $('.back-to-top').removeClass('show');
-    //         }
-    //     };
-    //     backToTop();
-    //     $(window).on('scroll', function () {
-    //         backToTop();
-    //     });
-    //     $('.back-to-top').on('click', function (e) {
-    //         e.preventDefault();
-    //         $('html,body').animate({
-    //             scrollTop: 0
-    //         }, 500);
-    //     });
-    // }
+    if ($('.back-to-top').length) {
+        let scrollTrigger = 100, // px
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('.back-to-top').addClass('show');
+            } else {
+                $('.back-to-top').removeClass('show');
+            }
+        };
+        backToTop();
+        $(window).on('scroll', function () {
+            backToTop();
+        });
+        $('.back-to-top').on('click', function (e) {
+            e.preventDefault();
+            $('html,body').animate({
+                scrollTop: 0
+            }, 500);
+        });
+    }
 
     // accordian education
     $(function() {
@@ -99,24 +100,24 @@ $(document).ready(function() {
 });
 
 ////////// wes bos grid gallery
-// const gallery = document.querySelector('.gallery-box');
-// const overlay = document.querySelector('.overlay-box');
-// const overlayImage = overlay.querySelector('img');
-// const overlayClose = overlay.querySelector('.close');
+const gallery = document.querySelector('.gallery-box');
+const overlay = document.querySelector('.overlay-box');
+const overlayImage = overlay.querySelector('img');
+const overlayClose = overlay.querySelector('.close');
 
-// function handleClick(e) {
-//     const src = e.currentTarget.querySelector('img').src;
-//     overlayImage.src = src;
-//     overlay.classList.add('open');
-// }
+function handleClick(e) {
+    const src = e.currentTarget.querySelector('img').src;
+    overlayImage.src = src;
+    overlay.classList.add('open');
+}
 
-// function close() {
-//     overlay.classList.remove('open');
-// }
+function close() {
+    overlay.classList.remove('open');
+}
 
-// const items = document.querySelectorAll('.item-flex');
-// items.forEach(item => item.addEventListener('click', handleClick));
-// overlayClose.addEventListener('click', close);
+const items = document.querySelectorAll('.item-flex');
+items.forEach(item => item.addEventListener('click', handleClick));
+overlayClose.addEventListener('click', close);
 
 
 
